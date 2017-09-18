add0687b0cf1775d9f3c5ed30f71d40719f7be13
Felix Krull <f_krull@gmx.de>
2017-09-18 20:28:15 +0200

    Provider.listAvailableExtensions

--- src/localexts/Provider.ts
+++ src/localexts/Provider.ts
@@ -1,4 +1,41 @@
+import * as fs from 'fs';
+import * as path from 'path';
 import LocalExtension from './LocalExtension';
+
+function promisify<R>(func): (...args: any[]) => Promise<R> {
+    return (...args) => {
+        return new Promise((resolve, reject) => {
+            const fullArgs = args.concat((error, result) => {
+                if (error !== null && error !== undefined) {
+                    reject(error);
+                } else {
+                    resolve(result);
+                }
+            });
+            func(...fullArgs);
+        });
+    };
+}
+
+const readdir: (path: string) => Promise<string[]> = promisify(fs.readdir);
+const stat: (path: string) => Promise<fs.Stats> = promisify(fs.stat);
+function access(filepath: string, mode: number): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+        fs.access(filepath, mode, (error) => {
+            resolve(error === null || error === undefined);
+        });
+    });
+}
+
+async function isReadableFile(filepath: string): Promise<boolean> {
+    try {
+        const stats = await stat(filepath);
+        const readable = await access(filepath, fs.constants.R_OK);
+        return stats.isFile() && readable;
+    } catch {
+        return false;
+    }
+}
 
 export default class Provider {
     private availableExtensionPath: string;
@@ -7,8 +44,20 @@ export default class Provider {
         this.availableExtensionPath = availableExtensionPath;
     }
 
-    public listAvailableExtensions(): Promise<LocalExtension[]> {
-        return Promise.resolve([]);
+    public async listAvailableExtensions(): Promise<LocalExtension[]> {
+        const availableExts = [];
+        const files = await readdir(this.availableExtensionPath);
+        for (const fname of files) {
+            const pkgjson = path.join(this.availableExtensionPath, fname, 'package.json');
+            const readable = await isReadableFile(pkgjson);
+            if (readable) {
+                availableExts.push({
+                    name: fname,
+                    path: path.join(this.availableExtensionPath, fname),
+                });
+            }
+        }
+        return availableExts;
     }
 
     public listInstalledExtensions(): Promise<LocalExtension[]> {
--- test/localexts/Provider.test.ts
+++ test/localexts/Provider.test.ts
@@ -2,6 +2,7 @@ import * as assert from 'assert';
 import * as fs from 'fs';
 import * as path from 'path';
 import * as tmp from 'tmp';
+import LocalExtension from '../../src/localexts/LocalExtension';
 import Provider from '../../src/localexts/Provider';
 
 suite('Provider.listAvailableExtensions', () => {
@@ -16,19 +17,44 @@ suite('Provider.listAvailableExtensions', () => {
     });
 
     test('should list all extensions in extension dir', async () => {
-        let expectedExts = [];
+        const expectedExts = [];
         for (const extName of ['ext4', 'ext2', 'ext1']) {
             const extdir = path.join(tmpdir.name, extName);
             fs.mkdirSync(extdir);
             const pkgjson = path.join(extdir, 'package.json');
             fs.writeFileSync(pkgjson, '{"version":"0.1.0"}');
-            expectedExts = expectedExts.concat({name: extName, path: extdir});
+            expectedExts.push({name: extName, path: extdir});
         }
         const provider = new Provider(tmpdir.name);
 
         const availableExts = await provider.listAvailableExtensions();
 
+        availableExts.sort(compareLocalExtensions);
+        expectedExts.sort(compareLocalExtensions);
         assert.deepEqual(availableExts, expectedExts);
     });
 
+    test('should not list directories without package.json', async () => {
+        fs.mkdirSync(path.join(tmpdir.name, 'ext'));
+        const provider = new Provider(tmpdir.name);
+
+        const availableExts = await provider.listAvailableExtensions();
+
+        assert.deepEqual(availableExts, []);
+    });
+
+    test('should not list directories where package.json is a directory', async () => {
+        fs.mkdirSync(path.join(tmpdir.name, 'ext'));
+        fs.mkdirSync(path.join(tmpdir.name, 'ext', 'package.json'));
+        const provider = new Provider(tmpdir.name);
+
+        const availableExts = await provider.listAvailableExtensions();
+
+        assert.deepEqual(availableExts, []);
+    });
+
 });
+
+function compareLocalExtensions(a: LocalExtension, b: LocalExtension): number {
+    return a.name < b.name ? -1 : (a.name > b.name ? 1 : 0);
+}
