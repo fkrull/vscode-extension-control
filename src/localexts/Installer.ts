2f666b90f055e4e5ec29d7079cd976e70af0625e
Felix Krull <f_krull@gmx.de>
2017-09-19 21:01:00 +0200

    Installer.install

--- src/localexts/Installer.ts
+++ src/localexts/Installer.ts
@@ -1,8 +1,21 @@
+import * as fs from 'fs-extra';
+import * as path from 'path';
 import LocalExtension from './LocalExtension';
 
 export default class Installer {
-    public install(exts: LocalExtension[]): Promise<void> {
-        return Promise.resolve();
+    private readonly extensionDir: string;
+
+    constructor(extensionDir: string) {
+        this.extensionDir = extensionDir;
+    }
+
+    public async install(localExts: LocalExtension[]): Promise<void> {
+        for (const localExt of localExts) {
+            const destPath = path.join(this.extensionDir, localExt.name);
+            await fs.copy(localExt.path, destPath);
+            const localExtFile = path.join(destPath, 'local-extension');
+            await fs.writeFile(localExtFile, localExt.path);
+        }
     }
 
     public uninstall(exts: LocalExtension[]): Promise<void> {
---
+++ test/localexts/Installer.test.ts
@@ -0,0 +1,51 @@
+import * as assert from 'assert';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as tmp from 'tmp';
+import Installer from '../../src/localexts/Installer';
+import LocalExtension from '../../src/localexts/LocalExtension';
+
+suite('Installer.install', () => {
+    let srcdir: tmp.SynchrounousResult;
+    let dstdir: tmp.SynchrounousResult;
+
+    setup(() => {
+        srcdir = tmp.dirSync({ unsafeCleanup: true });
+        dstdir = tmp.dirSync({ unsafeCleanup: true });
+    });
+
+    teardown(() => {
+        srcdir.removeCallback();
+        dstdir.removeCallback();
+    });
+
+    test('should copy specified folders and create local-extension file', async () => {
+        const localExts: LocalExtension[] = [];
+        for (const extName of ['ext2', 'ext1']) {
+            const srcPath = path.join(srcdir.name, extName);
+            fs.mkdirSync(srcPath);
+            const testFile = path.join(srcPath, `test-${extName}-file.js`);
+            fs.writeFileSync(testFile, `test-${extName}`);
+            localExts.push({ name: extName, path: srcPath });
+        }
+        fs.mkdirSync(path.join(srcdir.name, 'ext3'));
+        const installer = new Installer(dstdir.name);
+
+        await installer.install(localExts);
+
+        const dstNames = fs.readdirSync(dstdir.name);
+        dstNames.sort();
+        assert.deepEqual(dstNames, ['ext1', 'ext2']);
+        for (const localExt of localExts) {
+            const dstPath = path.join(dstdir.name, localExt.name);
+            const names = fs.readdirSync(dstPath);
+            names.sort();
+            assert.deepEqual(names, ['local-extension', `test-${localExt.name}-file.js`]);
+            const testFile = fs.readFileSync(path.join(dstPath, `test-${localExt.name}-file.js`));
+            assert.equal(testFile.toString(), `test-${localExt.name}`);
+            const localExtFile = fs.readFileSync(path.join(dstPath, 'local-extension'));
+            assert.equal(localExtFile.toString(), localExt.path);
+        }
+    });
+
+});
