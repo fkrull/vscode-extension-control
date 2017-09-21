27bfc242c8cc392109bc57a97cf0d2f906d6cea5
Felix Krull <f_krull@gmx.de>
2017-09-22 00:12:39 +0200

    DefaultPaths

---
+++ src/DefaultPaths.ts
@@ -0,0 +1,29 @@
+import * as os from 'os';
+import * as path from 'path';
+
+export default class DefaultPaths {
+    private readonly platform: NodeJS.Platform;
+
+    constructor(platform: NodeJS.Platform) {
+        this.platform = platform;
+    }
+
+    public getUserDirectory(): string {
+        return path.join(this.getBaseConfigDir(), 'Code', 'User');
+    }
+
+    public getExtensionDirectory(): string {
+        return path.join(os.homedir(), '.vscode', 'extensions');
+    }
+
+    private getBaseConfigDir(): string {
+        switch (this.platform) {
+            case 'win32':
+            return path.join(os.homedir(), 'AppData', 'Roaming');
+            case 'darwin':
+            return path.join(os.homedir(), 'Library', 'Application Support');
+            default:
+            return path.join(os.homedir(), '.config');
+        }
+    }
+}
---
+++ test/unit/DefaultPaths.ts
@@ -0,0 +1,54 @@
+import * as assert from 'assert';
+import * as os from 'os';
+import * as path from 'path';
+import DefaultPaths from '../../src/DefaultPaths';
+
+suite('DefaultPaths.getUserDirectory', () => {
+
+    const homeDir = os.homedir();
+
+    test('should get the default VSCode user directory for Windows', () => {
+        const paths = new DefaultPaths('win32');
+
+        assert.strictEqual(paths.getUserDirectory(), path.join(homeDir, 'AppData', 'Roaming', 'Code', 'User'));
+    });
+
+    test('should get the default VSCode user directory for Linux', () => {
+        const paths = new DefaultPaths('linux');
+
+        assert.strictEqual(paths.getUserDirectory(), path.join(homeDir, '.config', 'Code', 'User'));
+    });
+
+    test('should get the default VSCode user directory for macOS', () => {
+        const paths = new DefaultPaths('darwin');
+
+        assert.strictEqual(
+            paths.getUserDirectory(),
+            path.join(homeDir, 'Library', 'Application Support', 'Code', 'User'));
+    });
+
+});
+
+suite('DefaultPaths.getExtensionDirectory', () => {
+
+        const homeDir = os.homedir();
+
+        test('should get the default VSCode extension directory for Windows', () => {
+            const paths = new DefaultPaths('win32');
+
+            assert.strictEqual(paths.getExtensionDirectory(), path.join(homeDir, '.vscode', 'extensions'));
+        });
+
+        test('should get the default VSCode extension directory for Linux', () => {
+            const paths = new DefaultPaths('linux');
+
+            assert.strictEqual(paths.getExtensionDirectory(), path.join(homeDir, '.vscode', 'extensions'));
+        });
+
+        test('should get the default VSCode extension directory for macOS', () => {
+            const paths = new DefaultPaths('darwin');
+
+            assert.strictEqual(paths.getExtensionDirectory(), path.join(homeDir, '.vscode', 'extensions'));
+        });
+
+    });
