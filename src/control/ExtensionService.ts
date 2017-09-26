e0092b3f411d043cf03e84a63ad9596978ff2ef3
Felix Krull <f_krull@gmx.de>
2017-09-26 21:19:40 +0200

    ExtensionService

---
+++ src/control/ExtensionService.ts
@@ -0,0 +1,16 @@
+import IConfiguredExtension from '../config/IConfiguredExtension';
+import IInstalledExtension from '../installedextensions/IInstalledExtension';
+import IExtensionService from './IExtensionService';
+
+export default class ExtensionService implements IExtensionService {
+    public async selectExtensionsToInstall(
+        configuredExtensions: IConfiguredExtension[],
+        installedExtensions: IInstalledExtension[],
+    ): Promise<IConfiguredExtension[]> {
+        return configuredExtensions.filter((ext) => !this.extensionIsInstalled(ext, installedExtensions));
+    }
+
+    private extensionIsInstalled(ext: IConfiguredExtension, installedExtensions: IInstalledExtension[]): any {
+        return installedExtensions.findIndex((installedExt) => ext.id === installedExt.id) !== -1;
+    }
+}
---
+++ test/unit/control/ExtensionService.test.ts
@@ -0,0 +1,49 @@
+import * as assert from 'assert';
+
+import IConfiguredExtension from '../../../src/config/IConfiguredExtension';
+import ExtensionService from '../../../src/control/ExtensionService';
+
+suite('ExtensionService.selectExtensionsToInstall', () => {
+
+    const extService = new ExtensionService();
+
+    test('should return empty list if no configured extensions', async () => {
+        const configuredExts = [];
+        const installedExts = [
+            {id: 'ext1', extensionPath: 'ext1Path'},
+        ];
+
+        const extsToInstall = await extService.selectExtensionsToInstall(configuredExts, installedExts);
+
+        assert.deepEqual(extsToInstall, configuredExts);
+    });
+
+    test('should return all configured extensions if there are no installed extensions', async () => {
+        const configuredExts = [
+            {id: 'ext1', type: 'test'},
+        ];
+        const installedExts = [];
+
+        const extsToInstall = await extService.selectExtensionsToInstall(configuredExts, installedExts);
+
+        assert.deepEqual(extsToInstall, configuredExts);
+    });
+
+    test('should return configured extensions that are not the list of installed extensions', async () => {
+        const configuredExts = [
+            {id: 'ext1', type: 'test'},
+            {id: 'ext2', type: 'test'},
+        ];
+        const installedExts = [
+            {id: 'ext1', extensionPath: 'ext1Path'},
+            {id: 'ext3', extensionPath: 'ext3Path'},
+        ];
+
+        const extsToInstall = await extService.selectExtensionsToInstall(configuredExts, installedExts);
+
+        assert.deepEqual(extsToInstall, [
+            {id: 'ext2', type: 'test'},
+        ]);
+    });
+
+});
